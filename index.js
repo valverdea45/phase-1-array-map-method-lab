3851bc7dde5cd0dc378e52ecd6e2132453ec55ed
Adrian <adrianvalverde45@gmail.com>
2022-10-01 16:49:14 -0400

    commit tree 0c354d46d4e31a7c3898a575d23ca939b2dbc43a

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+  return tutorials.map(function (element) {
+    const splitting = element.split(" ")
+    const upperCaseLetters = splitting.map(function (character){
+      return character[0].toUpperCase() + character.slice(1)
+    })
+    return upperCaseLetters.join(" ")
+  })
 }
+
+console.log(titleCased())
+/*
+const titleCasedp = tutorials.map(function (element) {
+  const upperCaseLetters = element.map(function (character){
+    return character[0].toUpperCase() + character.slice[1]
+  })
+  return element + tutorials.slice[1]
+})
+*/
